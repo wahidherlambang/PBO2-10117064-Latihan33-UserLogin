4e0bb26e11c9ddba24a9559cb8e3846534a02ea3
Wahid Herlambang Suroso <43497169+wahidherlambang@users.noreply.github.com>
2018-10-08 20:21:50 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117064/latihan33/userlogin/PBO210117064Latihan33ObjectOrientedUserLogin.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan33.userlogin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ */
+public class PBO210117064Latihan33ObjectOrientedUserLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        User signin = new User();
+        Scanner scnr = new Scanner(System.in);
+        
+        System.out.print("Masukkan Username = ");
+        String user = scnr.next();
+        System.out.print("Masukkan Password = ");
+        String pass = scnr.next();
+        System.out.println();
+        
+        signin.pengecekanLogin(user,pass);
+    }
+    
+}
---
+++ src/pbo2/pkg10117064/latihan33/userlogin/User.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan33.userlogin;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ */
+public class User {
+    
+    public boolean status;
+    private static String db_user = "WahidHerlambang";
+    private static String db_pass = "Alhamdulillah";
+    
+    public void cekAkun(String username, String password) {
+        if(username.equals(db_user) && password.equals(db_pass)) {
+            status = true;
+        } else {
+            status = false;
+        }
+
+    }
+    public void hasilLogin(boolean status, String username) {
+        if(status) {
+            System.out.println("****** HALLO " + username.toUpperCase() + " ******");
+            System.out.println("Selamat Datang di Aplikasi ini.");
+        } else {
+            System.out.println("Ooops, Username atau Password anda salah");
+        }
+    }
+    public void pengecekanLogin(String username, String password) {
+        cekAkun(username,password);
+        hasilLogin(status,username);
+    }
+}
